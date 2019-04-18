8bcf299b35dc1a381b1d91c5e6518e40a1daecd3
fenikspp <pedro.cic.unb@gmail.com>
2019-04-18 13:13:01 -0300

    Usando o Cmake para gerar o makefile

---
+++ ValidaRomano.hpp
@@ -0,0 +1,22 @@
+//
+// Created by pedro on 18/04/19.
+//
+
+#ifndef TESTETRABALHO_VALIDAROMANO_HPP
+#define TESTETRABALHO_VALIDAROMANO_HPP
+
+#include "ConverteRomano.hpp"
+#include "ConverteInteiro.hpp"
+
+
+bool compara(int inteiro, string romano)
+{
+    if(romano == ConverteAuxToInteiro((romanToInt(romano))))
+    {
+        return true;
+    } else{
+        return false;
+    }
+}
+
+#endif //TESTETRABALHO_VALIDAROMANO_HPP
